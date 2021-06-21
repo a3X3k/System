205a5260df0cdd96be89be8689852699772c154b
S Abhishek <sabhishek@am.students.amrita.edu>
2021-06-21 20:02:35 +0530

    Add files via upload

---
+++ Forensics Setup/Setup.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+sudo apt update;
+sudo apt upgrade;
+sudo pip install python;
+sudo apt-get install python3-pip;
+sudo apt install git;
+sudo apt install snap;
+sudo apt install exiftool;
+sudo apt install ghex;
+sudo apt install binwalk;
+sudo apt install steghide;
+sudo apt-get install zbar-tools;
+sudo apt install gnupg;
+sudo snap install john-the-ripper;
+sudo apt-get install steghide -y;
+sudo pip3 install stegcracker;
+sudo apt install fcrackzip;
+sudo apt-get install pdfcrack;
+wget http://www.caesum.com/handbook/Stegsolve.jar -O stegsolve.jar;
+chmod +x stegsolve.jar;
+mkdir bin;
+mv stegsolve.jar bin/;
+sudo pip3 install stegoVeritas;
+stegoVeritas_install_deps;
+sudo apt install stegsnow
+sudo apt install pngcheck;
+sudo apt install wine-stable;
+sudo git clone https://github.com/jesparza/peepdf;
+sudo wget -O /usr/bin/jsteg https://github.com/lukechampine/jsteg/releases/download/v0.1.0/jsteg-linux-amd64;
+sudo chmod +x /usr/bin/jsteg;
+sudo wget -O /usr/bin/slink https://github.com/lukechampine/jsteg/releases/download/v0.2.0/slink-linux-amd64;
+chmod +x /usr/bin/slink;
+sudo apt install ruby;
+sudo gem install zsteg;
+sudo git clone https://github.com/volatilityfoundation/volatility.git; 
+sudo apt-get update;
+sudo apt-get install python3 python3-pip python3-dev git libssl-dev libffi-dev build-essential;
+python3 -m pip install --upgrade pip;
+python3 -m pip install --upgrade pwntools;
+
+sudo apt-get update;
+sudo apt-get install python python-pip python-dev git libssl-dev libffi-dev build-essential;
+python2 -m pip install --upgrade pip==20.3.4;
+python2 -m pip install --upgrade pwntools;
+sudo pip3 install autopep8;
